b3af207e805ec238086c0a0ba6312084668a6a9b
staroso <104366471+staroso@users.noreply.github.com>
2022-04-29 13:10:42 +0300

    добавлено задание 1

--- sandbox/src/main/java/dt/stqa/pft/sanbox/myfirstcode.java
+++ sandbox/src/main/java/dt/stqa/pft/sanbox/myfirstcode.java
@@ -1,10 +1,14 @@
 package dt.stqa.pft.sanbox;
+
 public class MyFirstCode {
 	public static void main(String[] args) {
-		hello("world");
-	}
-		public static void hello(String somebody) {
-		System.out.println("Hello, " + somebody +"!" );
 
-	}
+				Point p1 = new Point(2, 3);
+				Point p2 = new Point(3, 4);
+
+				System.out.println("Расстояние между точками  = " + distance(p1, p2));
+			}
+			public static double distance(Point p1, Point p2){
+				return Math.sqrt((p1.y-p2.y)*(p1.y-p2.y)+(p1.x-p2.x)*(p1.x-p2.x));
+			}
 }
