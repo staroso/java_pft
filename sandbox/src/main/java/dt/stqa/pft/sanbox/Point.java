5b2bb53ca481a207712b3495b9129d4d632b93ed
staroso <104366471+staroso@users.noreply.github.com>
2022-04-29 13:31:24 +0300

    задание2

--- sandbox/src/main/java/dt/stqa/pft/sanbox/Point.java
+++ sandbox/src/main/java/dt/stqa/pft/sanbox/Point.java
@@ -1,6 +1,5 @@
 package dt.stqa.pft.sanbox;
 
-
 public class Point {
   public double x;
   public double y;
@@ -9,6 +8,7 @@ public class Point {
     this.x = x;
     this.y = y;
   }
-
-
-}
+  public double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }
+  }
--- sandbox/src/main/java/dt/stqa/pft/sanbox/myfirstcode.java
+++ sandbox/src/main/java/dt/stqa/pft/sanbox/myfirstcode.java
@@ -6,9 +6,7 @@ public class MyFirstCode {
 				Point p1 = new Point(2, 3);
 				Point p2 = new Point(3, 4);
 
-				System.out.println("Расстояние между точками  = " + distance(p1, p2));
+		System.out.println("Расстояние между точками = " + p1.distance(p1, p2));
 			}
-			public static double distance(Point p1, Point p2){
-				return Math.sqrt((p1.y-p2.y)*(p1.y-p2.y)+(p1.x-p2.x)*(p1.x-p2.x));
-			}
+
 }
