0f6853e5d44d654e0a8b64d36b987bf7cc487c2e
staroso <104366471+staroso@users.noreply.github.com>
2022-05-02 13:25:49 +0300

    Добавлены тесты testDistance1, testDistance2.testDistance3

--- sandbox/src/test/java/dt/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/dt/stqa/pft/sandbox/PointTests.java
@@ -5,9 +5,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 public class PointTests {
   @Test
-  public void testArea() {
+  public void testDistance1() {
     Point p1 = new Point(2, 3);
     Point p2 = new Point(3, 4);
     Assert.assertEquals(p1.distance(p1, p2), 1.4142135623730951);
+
+  }
+
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p1, p2), 0);
+  }
+
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(-2, -3);
+    Point p2 = new Point(-4, -5);
+    Assert.assertEquals(p1.distance(p1, p2), 2.8284271247461903);
   }
 }
