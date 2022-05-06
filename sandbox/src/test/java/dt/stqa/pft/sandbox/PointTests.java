ff5d5887d8d5783b78276c97f4b236614005ecab
staroso <104366471+staroso@users.noreply.github.com>
2022-05-06 09:31:46 +0300

    поправила опечатку в PointTests

--- sandbox/src/test/java/dt/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/dt/stqa/pft/sandbox/PointTests.java
@@ -8,7 +8,7 @@ public class PointTests {
   public void testDistance1() {
     Point p1 = new Point(2, 3);
     Point p2 = new Point(3, 4);
-    Assert.assertEquals(p1.distance(p1), 1.4142135623730951);
+    Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
 
   }
 
@@ -16,13 +16,13 @@ public class PointTests {
   public void testDistance2() {
     Point p1 = new Point(0, 0);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(p1.distance(p1), 0);
+    Assert.assertEquals(p1.distance(p2), 0);
   }
 
   @Test
   public void testDistance3() {
     Point p1 = new Point(-2, -3);
     Point p2 = new Point(-4, -5);
-    Assert.assertEquals(p1.distance(p1), 2.8284271247461903);
+    Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
   }
 }
