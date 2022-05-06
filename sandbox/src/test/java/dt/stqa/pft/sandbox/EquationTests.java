bd7d79835c6c631ef0bc9242c5cf72e10a85b9bd
staroso <104366471+staroso@users.noreply.github.com>
2022-05-06 16:55:26 +0300

    добавлены классы Equation и EquationTests

---
+++ sandbox/src/test/java/dt/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,30 @@
+package dt.stqa.pft.sandbox;
+
+ import dt.stqa.pft.sanbox.Equation;
+ import org.testng.annotations.Test;
+
+ import static org.testng.Assert.assertEquals;
+
+
+public class EquationTests {
+
+  @Test
+  public void test0() {
+    Equation e = new Equation(1,1,1);
+    assertEquals(e.rootNumber(), 0);
+
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation(1,2,1);
+    assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void test2() {
+    Equation e = new Equation(1,5,6);
+    assertEquals(e.rootNumber(), 2);
+  }
+}
+
