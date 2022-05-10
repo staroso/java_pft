30e2a6ec07e983d067afbc39aac773a36279b6d9
staroso <104366471+staroso@users.noreply.github.com>
2022-05-10 21:56:53 +0300

    Добавлена возможноcть запускать тесты в разных браузерах (Chrome, IE, Firefox)

--- addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -14,8 +18,20 @@ public class ApplicationManager {
   private GroupsHelper groupsHelper;
   private ContactHelper contactHelper;
   private JavascriptExecutor js;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
+    if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+  } else if (browser == BrowserType.IE ) {
+      wd = new InternetExplorerDriver();
+    }
     System.setProperty("webdriver.chrome.driver", "C:\\Users\\nstaroso\\GitHub\\java_pft\\addressbook-web-tests\\chromedriver.exe");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
--- addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestBase.java
@@ -1,19 +1,16 @@
 package dt.stqa.pft.addressbook.tests;
 
 import dt.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 public class TestBase {
-
-  protected final ApplicationManager app = new ApplicationManager();
-
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
 
   }
-
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
