7baf47b0e3e5b1aeae78244032d9900be82544aa
staroso <104366471+staroso@users.noreply.github.com>
2022-05-16 16:26:27 +0300

    Добавлен getGroupCount

--- addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestGroupsCreation.java
+++ addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestGroupsCreation.java
@@ -3,14 +3,19 @@ package dt.stqa.pft.addressbook.tests;
 import dt.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class TestGroupsCreation extends TestBase {
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
+    int before = app.getGroupsHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupsHelper().initGroupCreation();
     app.getGroupsHelper().fillGroupForm(new GroupData("test4", "test4", "test4"));
     app.getGroupsHelper().submitGroupCreation();
+    int after = app.getGroupsHelper().getGroupCount();
+    assertEquals(after, before + 1);
     app.getGroupsHelper().returntoGroupPage();
     app.logout();
   }
