8b9430b668df39cb91680d96ca7e0123649acc7b
staroso <104366471+staroso@users.noreply.github.com>
2022-05-17 10:40:56 +0300

    Добавлены провернки на колическтво элементов в TestGroupsDeletion и  GroupModificationTests

--- addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -2,10 +2,14 @@ package dt.stqa.pft.addressbook.tests;
 
 import dt.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification () {
+    int before = app.getGroupsHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupsHelper().isThereAGroup()) {
       app.getGroupsHelper().createGroup(new GroupData("test4", "test4", "test4"));
@@ -15,5 +19,7 @@ public class GroupModificationTests extends TestBase {
     app.getGroupsHelper().fillGroupForm(new GroupData("test4", null, "test4"));
     app.getGroupsHelper().submitGroupModification();
     app.getGroupsHelper().returntoGroupPage();
+    int after = app.getGroupsHelper().getGroupCount();
+    assertEquals(after, before);
   }
 }
--- addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestGroupsDeletion.java
+++ addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestGroupsDeletion.java
@@ -3,10 +3,13 @@ package dt.stqa.pft.addressbook.tests;
 import dt.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class TestGroupsDeletion extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
+    int before = app.getGroupsHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupsHelper().isThereAGroup()) {
       app.getGroupsHelper().createGroup(new GroupData("test4", "test4", "test4"));
@@ -14,6 +17,8 @@ public class TestGroupsDeletion extends TestBase {
     app.selectGroup();
     app.getGroupsHelper().deleteSelectedGroup();
     app.getGroupsHelper().returntoGroupPage();
+    int after = app.getGroupsHelper().getGroupCount();
+    assertEquals(after, before - 1);
     app.logout();
   }
 
