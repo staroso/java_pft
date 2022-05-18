fe052f35b88b3d151fddd4cf2c87d7bb462bc424
staroso <104366471+staroso@users.noreply.github.com>
2022-05-18 15:01:49 +0300

    Добавлено Лямбда-выражения в TestGroupsCreation

--- addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestGroupsCreation.java
+++ addressbook-web-tests/src/test/java/dt/stqa/pft/addressbook/tests/TestGroupsCreation.java
@@ -2,19 +2,16 @@ package dt.stqa.pft.addressbook.tests;
 
 import dt.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
-
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-
 import static org.testng.Assert.assertEquals;
-
 public class TestGroupsCreation extends TestBase {
-
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupsHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test22", null, null);
     app.getGroupsHelper().initGroupCreation();
     app.getGroupsHelper().fillGroupForm(group);
     app.getGroupsHelper().submitGroupCreation();
@@ -22,13 +19,8 @@ public class TestGroupsCreation extends TestBase {
     List<GroupData> after = app.getGroupsHelper().getGroupList();
     assertEquals(after.size(), before.size() + 1);
     before.add(group);
-    int max = 0;
-    for (GroupData g :after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+    before.add(group);
     assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     app.logout();
   }
