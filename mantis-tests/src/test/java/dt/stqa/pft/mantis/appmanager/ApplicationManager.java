e137ae7782d6252ae7032038355c632276f58605
staroso <104366471+staroso@users.noreply.github.com>
2022-06-24 22:38:10 +0300

    8.3

--- mantis-tests/src/test/java/dt/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/dt/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,10 +1,13 @@
 package dt.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,12 +15,12 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  WebDriver wd;
 
-  private final Properties properties;
-  private WebDriver wd;
+  JavascriptExecutor js;
 
   private String browser;
-  private RegistrationHelper registrationHelper;
+  private final Properties properties;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -25,32 +28,11 @@ public class ApplicationManager {
   }
 
   public void init() throws IOException {
+
     String target = System.getProperty("target", "local");
+
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-  }
 
-  public void stop() {
-    if (wd != null) {
-      wd.quit();
-    }
-  }
-
-  public HttpSession newSession() {
-    return new HttpSession(this);
-  }
-
-  public String getProperty(String key) {
-    return properties.getProperty(key);
-  }
-
-  public RegistrationHelper registration() {
-    if (registrationHelper == null) {
-      registrationHelper = new RegistrationHelper(this);
-    }
-    return registrationHelper;
-  }
-
-  public WebDriver getDriver() {
     if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     } else if (browser.equals(BrowserType.FIREFOX)) {
@@ -58,8 +40,26 @@ public class ApplicationManager {
     } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
-      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-      wd.get(properties.getProperty("web.baseUrl"));
-            return wd;
-    }
+
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) wd;
+
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+  public void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  public  HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public Object getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
--- mantis-tests/src/test/java/dt/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/dt/stqa/pft/mantis/tests/TestBase.java
@@ -7,16 +7,17 @@ import dt.stqa.pft.mantis.appmanager.ApplicationManager;
 public class TestBase {
 
   protected static final ApplicationManager app
-          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-  @BeforeSuite
-  public void setUp() throws Exception {
+
+  @BeforeSuite(alwaysRun = true)
+  protected void setUp() throws Exception {
     app.init();
   }
 
   @AfterSuite(alwaysRun = true)
-
-  public void tearDown() {
+  public void tearDown() throws Exception {
+    app.logout();
     app.stop();
   }
 
